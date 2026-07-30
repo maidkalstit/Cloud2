54067d93a8d2b972c0d1f8e0577e5fc25ccacea3
maidkalsit <tung12342004@gmail.com>
2026-07-30 21:48:21 +0700

    dbt

---
+++ dbt_project/run_dbt.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Load environment variables (GCS_BUCKET_NAME, etc.)
+export $(grep -v '^#' ../.env | xargs)
+
+# Inject Iceberg Packages AND Full Iceberg Configuration into JVM Boot Arguments
+export PYSPARK_SUBMIT_ARGS="--packages org.apache.iceberg:iceberg-spark-runtime-4.0_2.13:1.11.0,com.google.cloud.bigdataoss:gcs-connector:hadoop3-2.2.19 \
+--conf spark.sql.extensions=org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions \
+--conf spark.sql.catalog.demo=org.apache.iceberg.spark.SparkCatalog \
+--conf spark.sql.catalog.demo.type=hadoop \
+--conf spark.sql.catalog.demo.warehouse=gs://${GCS_BUCKET_NAME}/warehouse \
+--conf spark.sql.defaultCatalog=demo \
+pyspark-shell"
+
+# Execute dbt
+dbt run --profiles-dir .
